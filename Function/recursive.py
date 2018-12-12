4bbe1568db08937db2b370c103ec14fc6b3af054
Sagit <ourfor@foxmail.com>
2018-12-12 13:30:56 +0800

    practice

--- Function/recursive.py
+++ Function/recursive.py
@@ -10,8 +10,20 @@ def Recur(n):
     else:
         return n*Recur(n-1)
 
+def BubbleSort(array):
+    for i in range(len(array),1,-1):
+        for j in range(0,i):
+            if array[j]>array[j+1]:
+                tmp=array[j]
+                array[j]=array[j+1]
+                array[j+1]=tmp
+    return array
+
 print("输入一个整数，求出这个数的阶乘")
 n=input("n=")
+array=[2,3,1,33,11,332]
+
 print(str(Recur(n)))
 
-
+for i in range(0,len(array)):
+    print(str(array[i]))
